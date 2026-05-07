206e16f589f427c688c68e086d319503f1747c0f
Livhuwani Pascal <mac@MACs-MacBook-Air.local>
2026-05-07 18:04:31 +0200

    fix(android): disable new architecture to stabilize map rendering

--- app.config.js
+++ app.config.js
@@ -13,7 +13,7 @@ module.exports = {
     icon: "./assets/images/icon.png",
     scheme: "a2blift",
     userInterfaceStyle: "dark",
-    newArchEnabled: true,
+    newArchEnabled: false,
     splash: {
       image: "./assets/images/splash-icon.png",
       resizeMode: "contain",
